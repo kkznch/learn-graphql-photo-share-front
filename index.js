3fc8001a2bd9b8a1fce968b370349fb9cef18d00
kkznch <kkznch710md@gmail.com>
2020-04-04 15:09:01 +0900

    [add]reactとgraphql組み合わせてみた

--- index.js
+++ index.js
@@ -1,58 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { request } from 'graphql-request';
 
 const url = 'http://localhost:4000/graphql';
 
-const requestQuery = async (query) => {
-  return await request(url, query);
-};
-
-const requestMutation = async (mutation, params) => {
-  return await request(url, mutation, params);
-};
-
-const queryUsers = async () => {
-  const query = `
-    query listUsers {
-      allUsers {
-        name
-        avatar
-      }
+const query = `
+  query listUsers {
+    allUsers {
+      name
+      avatar
     }
-  `;
-
-  try {
-    const data = await requestQuery(query);
-    console.info(data);
-  } catch (e) {
-    console.error('エラーだよ');
   }
-};
+`;
 
-const mutationUsers = async (count) => {
-  const mutation = `
-    mutation populate($count: Int!) {
-      addFakeUsers(count: $count) {
-        name
-      }
+const mutation = `
+  mutation populate($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
     }
-  `;
-
-  try {
-    const data = await requestMutation(mutation, { count });
-    console.info(data);
-  } catch (e) {
-    console.error('エラーだよ: ' + e);
   }
-};
+`;
 
-const arg = process.argv[2];
-switch (arg) {
-  case 'queryUsers':
-    queryUsers().then();
-    break;
-  case 'mutationUsers':
-    mutationUsers(10).then();
-    break;
-}
+const App = ({ users = [] }) =>
+  <div>
+    これはAppだよ
+    {users.map(user =>
+      <div key={user.githubLogin}>
+        <img src={user.avatar} alt="" />
+        {user.name}
+      </div>
+    )}
+    <button onClick={addUser}>Add User</button>
+  </div>;
 
+const render = ({ allUsers = [] }) =>
+  ReactDOM.render(
+    <App users={allUsers} />,
+    document.getElementById('root')
+  );
 
+const addUser = () =>
+  request(url, mutation, {count: 1})
+    .then(requestAndRender)
+    .catch(console.error);
+
+const requestAndRender = () =>
+  request(url, query)
+    .then(render)
+    .catch(console.error);
+
+requestAndRender();
